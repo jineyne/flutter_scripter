09da22b2bd43e8a199c4f16f61eea5b33b13c730
park-cj <jineyne@gmail.com>
2021-11-23 01:46:05 +0900

    feat: Add new values

--- lib/machine/value.dart
+++ lib/machine/value.dart
@@ -2,9 +2,8 @@ abstract class Value {
   bool get isBoolean => false;
   bool get isNumber => false;
   bool get isString => false;
+  bool get isFunction => false;
   bool get isNull => false;
-
-  String asString();
 }
 
 class BooleanValue extends Value {
@@ -16,13 +15,8 @@ class BooleanValue extends Value {
   BooleanValue(this.value);
 
   @override
-  String asString() {
-    return '$value';
-  }
-
-  @override
   String toString() {
-    return asString();
+    return '$value';
   }
 }
 
@@ -35,13 +29,8 @@ class NumberValue extends Value {
   NumberValue(this.value);
 
   @override
-  String asString() {
-    return value.toString();
-  }
-
-  @override
   String toString() {
-    return asString();
+    return value.toString();
   }
 }
 
@@ -54,13 +43,67 @@ class StringValue extends Value {
   StringValue(this.value);
 
   @override
-  String asString() {
+  String toString() {
     return value;
+  }
+}
+
+abstract class FunctionValue extends Value {
+  int argc;
+
+  @override
+  bool get isFunction => true;
+
+  FunctionValue(this.argc);
+
+  Value call(List<Value> args);
+}
+
+typedef ScriptFunctionType = Value Function(List<Value> args);
+
+class ExternalFunctionValue extends FunctionValue {
+  ScriptFunctionType value;
+
+  ExternalFunctionValue(this.value, int argc) : super(argc);
+
+  @override
+  Value call(List<Value> args) {
+    return value(args);
   }
 
   @override
   String toString() {
-    return asString();
+    return value.toString();
+  }
+}
+
+abstract class ProcedureValue extends Value {
+  int argc;
+
+  @override
+  bool get isFunction => true;
+
+  ProcedureValue(this.argc);
+
+  void call(List<Value> args);
+}
+
+typedef ScriptProcedureType = void Function(List<Value> args);
+
+class ExternalProcedureValue extends ProcedureValue {
+  ScriptProcedureType value;
+
+  ExternalProcedureValue(this.value, int argc) : super(argc);
+
+  @override
+  Value call(List<Value> args) {
+    value(args);
+    return NullValue();
+  }
+
+  @override
+  String toString() {
+    return value.toString();
   }
 }
 
@@ -69,12 +112,7 @@ class NullValue extends Value {
   bool get isNull => true;
 
   @override
-  String asString() {
-    return "null";
-  }
-
-  @override
   String toString() {
-    return asString();
+    return "null";
   }
 }
